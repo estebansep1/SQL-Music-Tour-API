6cfc7b69e8f6fd95ab7e39aaef10b83b8bb4f33a
Esteban Sepulveda <tebansd@icloud.com>
2023-10-17 21:54:58 -0700

    fixed env variable

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 // DEPENDENCIES
+require('dotenv').config();
 const express = require('express')
 const app = express()
 const { Sequelize } = require('sequelize')
@@ -8,16 +9,17 @@ const sequelize = new Sequelize({
     storage: process.env.PG_URI,
     dialect: 'postgres',
     username: 'postgres',
-    password: '$Ports4life'
-  })
-  
-try {
-    sequelize.authenticate() 
-    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+    password: process.env.PASSWORD
+});
 
-} catch(err) {
-    console.log(`Unable to connect to PG: ${err}`) 
-}
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log(`Connected with Sequelize at ${process.env.PG_URI}`);
+    })
+    .catch((err) => {
+        console.error(`Unable to connect to PG: ${err}`);
+    });
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()

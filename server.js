ff1682c105c0cbd49da098428a65be9e29c13215
Esteban Sepulveda <tebansd@icloud.com>
2023-10-17 19:42:04 -0700

    final solution/ connected to postgreSQL

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const { Sequelize } = require('sequelize')
+
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize({
+    storage: process.env.PG_URI,
+    dialect: 'postgres',
+    username: 'postgres',
+    password: '$Ports4life'
+  })
+  
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
